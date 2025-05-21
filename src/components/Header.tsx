d35e13d35667c4655f1c61c766b9f0a639bb6aba
Timor malul <timor34@gmail.com>
2025-05-21 18:54:07 +0300

    Fix: added missing useState/useEffect imports

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-
+import { useState, useEffect } from "react";
 import { ShoppingCart, Menu } from "lucide-react";
 import { useCart } from "../context/CartContext";
 import CartModal from "./CartModal";
